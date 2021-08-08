17dfe953ef78efc3f2def7221a4e533d257befd6
nardiyansah <janatri1398@gmail.com>
2021-08-08 17:35:58 +0700

    [refactor] move detail user model instance to before each

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,39 +3,29 @@ require_relative '../../models/user'
 RSpec.describe User do
 
     describe "initialize object class" do
+        before(:each) do
+            @username = 'foo'
+            @email = 'foo@mail.com'
+    
+            @user = User.new(@username, @email)
+        end
+
         it 'should has username' do
-            username = 'foo'
-            email = 'foo@mail.com'
-
-            user = User.new(username, email)
-
-            expect(user.username).to eq(username)
+            expect(@user.username).to eq(@username)
         end
 
         it 'should has email' do
-            username = 'foo'
-            email = 'foo@mail.com'
-
-            user = User.new(username, email)
-
-            expect(user.email).to eq(email)
+            expect(@user.email).to eq(@email)
         end
 
         it 'bio can be nil' do
-            username = 'foo'
-            email = 'foo@mail.com'
-
-            user = User.new(username, email)
-
-            expect(user.bio).to be(nil)
+            expect(@user.bio).to be(nil)
         end
 
         it 'can contain bio' do
-            username = 'foo'
-            email = 'foo@mail.com'
             bio = 'onegaishimasu'
 
-            user = User.new(username, email, bio)
+            user = User.new(@username, @email, bio)
 
             expect(user.bio).to eq(bio)
         end
