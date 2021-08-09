91734279b32a7fda876c9c3c817ef8e8e1fbcfdc
nardiyansah <janatri1398@gmail.com>
2021-08-09 13:10:42 +0700

    [test] user controller must can insert data to database

--- controllers/userController.rb
+++ controllers/userController.rb
@@ -1,7 +1,22 @@
+require_relative '../models/user'
+
 class UserController
     attr_reader :db_client
-    
+
     def initialize(db_client)
         @db_client = db_client
+    end
+
+    def create(data)
+        if data['username'].nil? or data['username'].empty? or data['email'].nil? or data['email'].empty?
+            return "username or email not correct"
+        end
+
+        username = data['username']
+        email = data['email']
+        bio = data['bio']
+        
+        model = User.new(username, email, bio, nil, @db_client)
+        model.save
     end
 end
--- spec/controllers/user_spec.rb
+++ spec/controllers/user_spec.rb
@@ -5,12 +5,33 @@ require 'mysql2'
 RSpec.describe 'user controller' do
     db_client = create_db_client
 
+    before(:each) do
+        db_client.query('SET FOREIGN_KEY_CHECKS = 0')
+        db_client.query('TRUNCATE users')
+        db_client.query('SET FOREIGN_KEY_CHECKS = 1')
+    end
+
     describe 'initialize controller' do
         it 'should contain db client that will be pass to model' do
             controller = UserController.new(db_client)
 
             expect(controller.db_client).to_not be_nil
             expect(controller.db_client).to be_an_instance_of(Mysql2::Client)
+        end
+    end
+
+    describe 'create new account' do
+        it 'should create account' do
+            data = {"username" => "foo", "email" => "foo@mail.com"}
+            controller = UserController.new(db_client)
+
+            controller.create(data)
+            id = db_client.last_id
+
+            stored_data = db_client.query("SELECT * FROM users WHERE id = #{id}").each[0]
+
+            expect(stored_data['username']).to eq(data['username'])
+            expect(stored_data['email']).to eq(data['email'])
         end
     end
 end
